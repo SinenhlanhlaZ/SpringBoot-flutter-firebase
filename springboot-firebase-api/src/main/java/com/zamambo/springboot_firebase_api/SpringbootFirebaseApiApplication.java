68b41ac06e4d7c496eac33038b6fa0e100f711dc
Nhlanhla Mkhize <mbmkhize22@gmail.com>
2024-12-19 23:44:53 +0200

    first commit

---
+++ springboot-firebase-api/src/main/java/com/zamambo/springboot_firebase_api/SpringbootFirebaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.zamambo.springboot_firebase_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFirebaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFirebaseApiApplication.class, args);
+	}
+
+}
---
+++ springboot-firebase-api/src/test/java/com/zamambo/springboot_firebase_api/SpringbootFirebaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zamambo.springboot_firebase_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirebaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
